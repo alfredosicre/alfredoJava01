453dd8baa06627306ac071c010e468b032f16b0f
alfredosicre <alfredo.sicre@gmail.com>
2025-07-23 09:24:51 +0200

    23julio202501

--- src/j14_ejerciciojpa/src/main/java/com/cursogetafe/ejerciciojpa/consultas/Consulta6.java
+++ src/j14_ejerciciojpa/src/main/java/com/cursogetafe/ejerciciojpa/consultas/Consulta6.java
@@ -1,31 +1,34 @@
 package com.cursogetafe.ejerciciojpa.consultas;
 
+
 import java.util.List;
 
 import com.cursogetafe.ejerciciojpa.config.Config;
-import com.cursogetafe.ejerciciojpa.modelo.Cliente;
+import com.cursogetafe.ejerciciojpa.modelo.ClienteCategoria;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
+
 
 public class Consulta6 {
 	
 	public static void main(String[] args) {
 		
+		cantClientesPorCategoria().forEach(System.out::println);
+	}
+	
+	public static List<ClienteCategoria> cantClientesPorCategoria(){
+		
 		EntityManager em = Config.getEmf().createEntityManager();
 		
 		// buscar la cantidad de clientes por categoria
 		
-		String jpql = "select count(c.idRol), c.categoria from Cliente c group by c.categoria"; 
+//		String jpql = "select new com.cursogetafe.ejerciciojpa.modelo.ClienteCategoria(c.categoria, count(c.idRol)) from Cliente c group by c.categoria"; 
+		String jpql = "select c.categoria, count(c.idRol) from Cliente c group by c.categoria"; 
 		
-		List<Object[]> lista = em.createQuery(jpql, Object[].class).getResultList();
+		TypedQuery<ClienteCategoria> q = em.createQuery(jpql, ClienteCategoria.class);
 		
-		for (Object[] objects : lista) {
-			
-			System.out.println(objects[0] + " : " + objects[1]);
-			
-		}
-		
-	}
+		return q.getResultList();
 
+	}
 }
