8a93c081498fa70d97fcbe708609fafaeabe4e5a
alfredosicre <alfredo.sicre@gmail.com>
2025-07-30 12:39:17 +0200

    30julio202503

--- src/j17_threads/src/threads/productorconsumidor/ThPc03.java
+++ src/j17_threads/src/threads/productorconsumidor/ThPc03.java
@@ -1,7 +1,5 @@
 package threads.productorconsumidor;
 
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -15,73 +13,47 @@ public class ThPc03 {
 	private static final int CAPACIDAD = 5; // una cola de 5 elementos como maximo
 	private static final LinkedBlockingQueue<Integer> buffer = new LinkedBlockingQueue<Integer>(CAPACIDAD);
 				
-	private static final int CANT_PROD = 3;
-	private static final int CANT_CONS = 7;
+	private static final int CANT_PROD = 6;
+	private static final int CANT_CONS = 2;
 	
 	private static AtomicInteger valor = new AtomicInteger(0); // metodos para la sincronizacion de hilos
 
 
 	public static void main(String[] args) {
 		
-		ExecutorService executor = Executors.newFixedThreadPool(8);
+		ExecutorService executor = Executors.newFixedThreadPool(8); // trabaja con 8 hilos
 		
+		System.out.println(Runtime.getRuntime().availableProcessors()); // me dice cuantos procesadores tengo
+		System.out.println("-------------------------------------------");
 		
 		Runnable productor = ()->{ // cola de entrada
+			ThreadUtil.sleep();
 			while(true) {
-				ThreadUtil.sleep();
-				synchronized (LOCK) {
-					
-					while(buffer.size() == CAPACIDAD) {
-						try {
-							LOCK.wait();
-						}catch (InterruptedException e){
-							e.printStackTrace();
-						}
-					}
-					
-					buffer.offer(valor);
-					System.out.println(Thread.currentThread().getName() + " producto: " + valor);
-					valor++;
-					LOCK.notifyAll();
-					
-					// control innecesario
-					if(buffer.size() > CAPACIDAD) {
-						System.err.println("¡¡¡ Error !!! se produjeron " + buffer.size());
-						System.exit(1);
-					}
-				}
+				int v = valor.getAndIncrement();
+				buffer.add(v);
+				System.out.println(Thread.currentThread().getName() + " produjo: " + v);
 			}
 		};
 		
 		Runnable consumidor = () -> { // cola de salida
 			while(true) {
 				ThreadUtil.sleep();
-				synchronized (LOCK) {
-					while(buffer.size()== 0) {
-						try {
-							LOCK.wait();
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-					}
-					int v = buffer.poll();
+				try {
+					int v = buffer.take();
 					System.out.println(Thread.currentThread().getName() + " consumió: " + v);
-					
-					if(buffer.size() < 0) {
-						System.err.println("Error, el tamaño del buffer es " + buffer.size());
-						System.exit(1);
-					}
-				}
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}	
 			}
 		};
 		
 		
 		for(int i = 1; i <= CANT_PROD; i++) {
-			new Thread(productor, " productor: " + i).start();
+			executor.submit(productor);
 		}
 		
 		for(int i = 1; i <= CANT_CONS; i++) {
-			new Thread(consumidor, " consumidor: " + i).start();
+			executor.submit(consumidor);
 		}
 	}
 
